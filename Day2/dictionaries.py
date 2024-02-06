08f2f4194841bd1377124f3af4efbb102982a923
Josh <goldbergjosh55@gmail.com>
2024-02-06 12:28:52 +0000

    first challenge complete

--- Day2/dictionaries.py
+++ Day2/dictionaries.py
@@ -1,12 +1,45 @@
-animals = {
-    "Lion" : "Cub",
-    "Cat" : "Kitten",
-    "Dog" : "Puppy",
-    "Horse" : "Foal"
+# animals = {
+#     "Lion" : "Cub",
+#     "Cat" : "Kitten",
+#     "Dog" : "Puppy",
+#     "Horse" : "Foal"
+# }
+
+# print(animals["Dog"])
+
+# animals["Dog"] = "Hatchling"
+
+# print (animals["Dog"])
+
+# print(animals.keys())
+
+#challenge 1 and stretch
+
+favourite_songs = {  #create dictionary of favourite songs
+    "Pink Floyd" : "Pigs",
+    "Rammstein" : "Deutchsland",
+    "Metallica" : "Fade to Black",
+    "Claptone" : "No Eyes",
+    "Eminem" : "Superman"
 }
 
-print(animals["Dog"])
+favourite_songs.setdefault("Polyphia", "Playing God") #add item to dictionary
+favourite_songs.setdefault("Chris Stapleton", "Tennessee Whiskey") #add item to dictionary
 
-animals["Dog"] = "Hatchling"
+for key, value in favourite_songs.items(): #uses for loop to print song list from dictionary
+    print(key, ':', value)
 
-print (animals["Dog"])
+
+#challenge 2
+
+
+#using the two lists, create a dictionary where country is the key and language is the value
+
+#using your dictionary of animals, create a program which allows a user to search for an animal to see the corresponding young name
+
+#if the animal does not exist in the dictionary, return a suitable message
+
+#using your dictionary of animals, create a program which allows a user to submit an animal name and baby name 
+#if the animal already exists, return the exisiting baby name
+#if the animal doesn't exist, add it to the dictionary
+
