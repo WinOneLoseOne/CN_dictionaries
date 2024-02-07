307aa315487669ced4ca0fdeec54f2387d5a68f9
Josh <goldbergjosh55@gmail.com>
2024-02-07 14:31:43 +0000

    challenge 3

--- Day3/challenges.py
+++ Day3/challenges.py
@@ -19,17 +19,53 @@
 # If the name appears on the barred list, the guest must
 # get turned away.
     
-guest_list = ["sophia", "olivia", "emma", "jackson", "isabella", "lucas"]
+# guest_list = ["sophia", "olivia", "emma", "jackson", "isabella", "lucas"]
 
-banned_list = ["ava", "aiden"]
+# banned_list = ["ava", "aiden"]
 
-while True:
-    enter_name = input("Type your name here: ").lower()
-    if enter_name in guest_list:
-        print("You can come in")
-    elif enter_name in banned_list:
-        print("You shall not pass")
+# while True:
+#     enter_name = input("Type your name here: ").lower()
+#     if enter_name in guest_list:
+#         print("You can come in")
+#     elif enter_name in banned_list:
+#         print("You shall not pass")
+#     else:
+#         end = input("You have to wait, would you like to enter another name? (y/n)" )
+#         if end != "y":
+#             break
+
+# Challenge 3
+# Create a cash machine function (or functions!).
+# The user must enter a correct pin and have enough
+# funds to be allowed to withdraw an amount.
+# Be creative.
+# Once you have this functionality, expand!
+# You could include more options, add a limit to the
+# number of times the user can guess their pin, and more!
+        
+pin = 1234
+balance = 1000
+pin_attempts = 3
+
+def my_pin():
+    return int(input("Enter a pin: "))
+
+def withdraw_amount():
+    return int(input("How much would you like to withdraw?: "))
+
+while pin_attempts != 0:
+    entered_pin = my_pin()
+    if entered_pin == pin:
+        withdrawal_amount = withdraw_amount()
+        if withdrawal_amount <= balance:
+            balance -= withdrawal_amount
+            print(f"Withdrawal successful! Your new balance is {balance}")
+        else:
+            print("Insufficient funds!")
     else:
-        end = input("You have to wait, would you like to enter another name? (y/n)" )
-        if end != "y":
-            break
+        print("Wrong pin, try again.")
+        pin_attempts -= 1
+
+    if pin_attempts == 0:
+        print("You've exceeded the maximum number of attempts.")
+
