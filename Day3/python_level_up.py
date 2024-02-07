d36f395a301d0031997e9e236456a3b9c9bd8081
Josh <goldbergjosh55@gmail.com>
2024-02-07 11:07:35 +0000

    for in lists

--- Day3/python_level_up.py
+++ Day3/python_level_up.py
@@ -2,14 +2,34 @@
 #generate 6 random numbers between these values and sorts them in order of highest to lowest
 #print
 
-import random
+# import random
 
-integer_1 = int(input("Enter your first integer: "))
-integer_2 = int(input("Enter your second integer: "))
+# integer_1 = int(input("Enter your first integer: "))
+# integer_2 = int(input("Enter your second integer: "))
 
-num_range = range(integer_1, integer_2)
+# num_range = range(integer_1, integer_2)
 
-for i in num_range:
-    num_range2 = random.randint(integer_1, integer_2)
-    print(num_range2)
+# for i in num_range:
+#     num_range2 = random.randint(integer_1, integer_2)
+#     print(num_range2)
 
+dinosaurs = [
+    "Triceratops",
+    "Velociraptor",
+    "Ankylosaurus",
+    "Archaeopteryz",
+    "Tyrannosaurus Rex",
+    "Stegosaurus",
+    "Iguanodon"
+]
+
+# saurus_dinosaurs = []
+
+# for i in dinosaurs:
+#     if "saurus" in i:
+#         saurus_dinosaurs.append(i)
+
+# print(saurus_dinosaurs)
+
+saurus_dinosaurs=[i for i in dinosaurs if "saurus" in i]
+print (saurus_dinosaurs)
